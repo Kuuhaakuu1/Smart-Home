7363a13c2ed9b4f2e0890464a444f9412e8385c2
Omar Temsamani <omartemsamani22@gmail.com>
2022-12-12 11:51:32 +0100

    First commit

---
+++ SmartHome/SmartHome/House.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace SmartHome
+{
+    /// <summary>
+    /// Interaction logic for House.xaml
+    /// </summary>
+    public partial class House : Window
+    {
+        MainWindow mainWindow;
+        
+        public House(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            
+            this.mainWindow = mainWindow;
+        }
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            mainWindow.Show();
+           
+
+        }
+
+        private void ToiletRoom_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragDrop.DoDragDrop(ToiletRoom,ToiletRoom,DragDropEffects.Move);
+            }
+        }
+
+        private void Border_Drop(object sender, DragEventArgs e)
+        {
+
+            Border clonedCanvas = Clone<Border>(ToiletRoom);
+            //clonedCanvas.Width = ToiletRoom.Width;
+            //clonedCanvas.Height = ToiletRoom.Height;
+            //set the position of the cloned canvas
+            //Point p = e.GetPosition(borderTest);
+            //borderTest.Children.Add(clonedCanvas);
+            //clonedCanvas.Margin = new Thickness(ToiletRoom.Margin.Left, ToiletRoom.Margin.Top, ToiletRoom.Margin.Right, ToiletRoom.Margin.Bottom);
+            //get the position of the mouse and set the position of the cloned canvas
+            Point p = e.GetPosition(borderTest);
+            clonedCanvas.Width = ToiletRoom.ActualWidth;
+            clonedCanvas.Height = ToiletRoom.ActualHeight;
+            clonedCanvas.Margin = new Thickness(p.X-190, p.Y-190, 710 - p.X, 480 - p.Y);
+
+            borderTest.Children.Add(clonedCanvas);
+        
+
+
+        //710,490
+        //clonedCanvas.Margin = new Thickness { Left = p.X, Top = p.Y, Right = 190 + p.X, Bottom = 190 + p.Y };
+
+
+
+
+    }
+
+
+        public static T Clone<T>(T source)
+        {
+            string objXaml = XamlWriter.Save(source);
+            StringReader stringReader = new StringReader(objXaml);
+            XmlReader xmlReader = XmlReader.Create(stringReader);
+            T t = (T)XamlReader.Load(xmlReader);
+            return t;
+        }
+
+        //public static Canvas CloneXaml(Canvas source)
+        //{
+        //    string xaml = XamlWriter.Save(source);
+        //    StringReader sr = new StringReader(xaml);
+        //    XmlReader xr = XmlReader.Create(sr);
+        //    return (Canvas)XamlReader.Load(xr);
+        //}
+
+
+
+
+
+    }
+}
---
+++ SmartHome/SmartHome/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartHome
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MySqlConnection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+             connection = new MySqlConnection("server=localhost;user id=root;database=library");
+
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand("SELECT * FROM user where username='" + TxtUsername.Text + "' and password='" + TxtPassword.Password.ToString() + "';", connection);
+                MySqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+
+                    if (TxtUsername.Text == (string)reader[3] && TxtPassword.Password.ToString() == (string)reader[4])
+                    {
+                        MessageBox.Show("Welcome back " + reader[2] + " " + reader[1], "Congrats", MessageBoxButton.OK, MessageBoxImage.Information);
+                        House house = new House(this);
+                        house.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Username or Password is incorrect", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Username or Password is incorrect", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            
+           
+            
+
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch (Exception ex)
+            {
+                //code for any other type of exception
+            }
+
+        }
+
+        private void btxExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
